a2f14dcf88722786d23b880463510f124404bb72
Shadowstrorm10 <75273905+Adityaraizada1@users.noreply.github.com>
2024-12-28 23:21:02 +0530

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  rules: {
+    "react/no-unescaped-entities": "off", // Disable unescaped characters rule
+    "@typescript-eslint/no-unused-vars": "off", // Disable unused vars rule
+  },
+};
