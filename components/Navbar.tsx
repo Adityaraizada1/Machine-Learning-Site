565e9a7318667293099a4f3794c3e28ee6a5b688
Shadowstrorm10 <75273905+Adityaraizada1@users.noreply.github.com>
2025-01-04 14:49:24 +0530

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -43,7 +43,7 @@ export default function App() {
   const [dropdownOpen, setDropdownOpen] = useState(false);
 
   // Toggle dropdown visibility
-  function toggleDropdown(_e: React.MouseEvent<HTMLButtonElement, MouseEvent>): void {
+  function toggleDropdown(): void {
     setDropdownOpen(!dropdownOpen);
   }
 
